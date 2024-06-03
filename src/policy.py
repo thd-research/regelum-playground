c9c09253b8337e0e2fceb713dc1c62066695b0a2
Huy Hoang <ngoc.hoang@stud.th-deg.de>
2024-06-03 17:56:32 +0200

    add nomial controller and customized system

--- src/policy.py
+++ src/policy.py
@@ -413,3 +413,55 @@ class ThreeWheeledRobotDynamicMinGradCLF(ThreeWheeledRobotKinematicMinGradCLF):
         action = -self.gain * (force_and_moment - three_wheeled_robot_kin_action)
 
         return action
+
+
+class ThreeWheeledRobotNomial(Policy):
+    def __init__(
+        self,
+        optimizer_config: CasadiOptimizerConfig,
+        action_bounds: list[list[float]],
+        eps=0.01,
+    ):
+        super().__init__(optimizer_config=optimizer_config)
+        self.action_bounds = action_bounds
+        # An epsilon for numerical stability
+        self.eps = eps
+        self.update_kappa(2, 15, -1.5)
+
+    def update_kappa(self, k_rho, k_alpha, k_beta):
+        # Parameters for gazebo
+        self.k_rho = k_rho
+        self.k_alpha = k_alpha  
+        self.k_beta = k_beta
+
+
+    def get_action(self, observation: np.ndarray):
+        x_robot = observation[0, 0]
+        y_robot = observation[0, 1]
+        theta = observation[0, 2]
+
+        x_goal = 0
+        y_goal = 0
+        theta_goal = 0
+
+        error_x = x_goal - x_robot
+        error_y = y_goal - y_robot
+        error_theta = theta_goal - theta
+
+        rho = np.sqrt(error_x**2 + error_y**2)
+        alpha = -theta + np.arctan2(error_y, error_x)
+        beta = error_theta - alpha
+
+        w = self.k_alpha*alpha + self.k_beta*beta
+        v = self.k_rho*rho
+
+        while alpha > np.pi:
+            alpha -= 2* np.pi
+
+        while alpha < -np.pi:
+            alpha += 2* np.pi
+
+        if -np.pi < alpha <= -np.pi / 2 or np.pi / 2 < alpha <= np.pi:
+            v = -v
+        
+        return np.array([[v, w]])
--- src/system.py
+++ src/system.py
@@ -10,6 +10,7 @@ from .animation import (
     ThreeWheeledRobotAnimationWithSpot,
 )
 from regelum.callback import detach
+from regelum.system import System
 
 
 # In the following two classes we want to alter their respective animation callbacks, so we:
@@ -30,13 +31,49 @@ class MyThreeWheeledRobotDynamic(ThreeWheeledRobotDynamic):
     action_bounds = [[-1, 1], [-1, 1]]
 
 
+# @ThreeWheeledRobotAnimationWithNewLims.attach
+# @DefaultAnimation.attach
+# @detach
+# class MyThreeWheeledRobotKinematic(ThreeWheeledRobotKinematic):
+#     """The parameters correspond to those of Robotis TurtleBot3."""
+
+#     action_bounds = [[-0.22, 0.22], [-2.84, 2.84]]
+
+
 @ThreeWheeledRobotAnimationWithNewLims.attach
 @DefaultAnimation.attach
 @detach
-class MyThreeWheeledRobotKinematic(ThreeWheeledRobotKinematic):
-    """The parameters correspond to those of Robotis TurtleBot3."""
+class MyThreeWheeledRobotKinematic(System):
+    """Kinematic three-wheeled robot system implementation. """
 
+    # These private variables are leveraged 
+    # by other components within the codebase.
+
+    # While optional, naming variables 
+    # enhance usability, especially for plotting.
+
+    _name = 'ThreeWheeledRobotKinematic'
+    _system_type = 'diff_eqn'
+    _dim_state = 3
+    _dim_inputs = 2
+    _dim_observation = 3
+    _observation_naming = _state_naming = ["x_rob", "y_rob", "vartheta"]
+    _inputs_naming = ["v", "omega"]
     action_bounds = [[-0.22, 0.22], [-2.84, 2.84]]
+
+    def _compute_state_dynamics(self, time, state, inputs):
+        """ Calculate the robot's state dynamics. """
+
+        # Placeholder for the right-hand side of the differential equations
+        Dstate = rg.zeros(self._dim_state, prototype=state) #
+
+        # Element-wise calculation of the Dstate vector 
+        # based on the system's differential equations
+        Dstate[0] = inputs[0] * rg.cos(state[2])  # v * cos(vartheta)
+        Dstate[1] = inputs[0] * rg.sin(state[2])  # v * sin(vartheta)
+        Dstate[2] = inputs[1]                     # omega
+
+        return Dstate
 
 
 @ThreeWheeledRobotAnimationWithSpot.attach
