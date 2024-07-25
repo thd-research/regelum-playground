fc565e1f88edc7eaf9530cea51c4504817d9acfc
Huy Hoang <ngoc.hoang@stud.th-deg.de>
2024-07-25 13:42:40 +0200

    add seed

--- launch.sh
+++ launch.sh
@@ -2,10 +2,11 @@
 # python3.10 run.py policy=3wrobot_kin_nomial initial_conditions=3wrobot_kin system=3wrobot_kin common.sampling_time=0.01 --interactive --fps=10 --jobs=1
 # python run.py policy=3wrobot_kin_nomial initial_conditions=3wrobot_kin system=3wrobot_kin common.sampling_time=0.01 --interactive --fps=10 --jobs=1 --configure
 
-python3.10 run.py policy=rc_calfq \
+python3.10 run.py +seed=range\(10,20\) \
+                  policy=rc_calfq \
                   initial_conditions=3wrobot_kin_with_spot \
                   scenario=my_scenario \
                   system=3wrobot_kin_with_spot \
                   common.sampling_time=0.1 \
                   simulator.time_final=10 scenario.N_iterations=100 \
-                  --interactive --fps=10
+                  --jobs=-1
