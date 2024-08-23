7d827842895bbc4ff7b03ba14b4f0308064c59e3
HuyHoang <huyhoang251296@gmail.com>
2024-08-23 21:55:38 +0200

    add sarsa_m script

--- scripts/launch_sarsa_m.sh
+++ scripts/launch_sarsa_m.sh
@@ -6,18 +6,19 @@ if [[ $1 = "--ros" ]] || [[ $1 = "-r" ]]
                   simulator=ros \
                   policy=rc_sarsa_m \
                   initial_conditions=3wrobot_kin_with_spot \
-                  +policy.R1_diag="[1, 100, 1e-1, 0, 0]" \
+                  +policy.R1_diag="[10, 100, 1e-1, 0, 0]" \
                   scenario=my_scenario \
                   system=3wrobot_kin_with_spot \
                   common.sampling_time=0.1 \
-                  simulator.time_final=25 scenario.N_iterations=20 \
+                  simulator.time_final=50 scenario.N_iterations=50 \
                   --jobs=-1 \
                   --experiment=sarsa_m_init_ros \
                   policy.critic_desired_decay=1e-6 \
                   policy.critic_low_kappa_coeff=1e-1 \
-                  policy.critic_up_kappa_coeff=1e3 \
+                  policy.critic_up_kappa_coeff=5e2 \
                   policy.penalty_factor=1e3 \
-                  policy.step_size_multiplier=5
+                  policy.step_size_multiplier=5 \
+                  --interactive
     done
     else
         python3.10 run.py +seed=7 \
