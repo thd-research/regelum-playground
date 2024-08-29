146c0fbe2925bea3c11c44a272493c95726ea6ca
HuyHoang <huyhoang251296@gmail.com>
2024-08-29 16:30:30 +0200

    add SARSA-m

--- scripts/launch_calf.sh
+++ scripts/launch_calf.sh
@@ -1,6 +1,6 @@
 if [[ $1 = "--ros" ]] || [[ $1 = "-r" ]]
     then 
-    for seed in {5..20}; do
+    for seed in {9..20}; do
         python3.10 run.py \
                   +seed=$seed \
                   simulator=ros \
@@ -11,12 +11,13 @@ if [[ $1 = "--ros" ]] || [[ $1 = "-r" ]]
                   system=3wrobot_kin_with_spot \
                   common.sampling_time=0.1 \
                   simulator.time_final=40 scenario.N_iterations=40 \
+                  --single-thread \
                   --jobs=-1 \
                   --experiment=calf_report \
                   policy.critic_desired_decay=1e-6 \
                   policy.critic_low_kappa_coeff=1e-1 \
                   policy.critic_up_kappa_coeff=1e3 \
-                  policy.penalty_factor=1e3 \
+                  policy.penalty_factor=1e2 \
                   policy.step_size_multiplier=5 \
                   policy.nominal_only=False
                 #   --interactive
--- scripts/launch_sarsa_m.sh
+++ scripts/launch_sarsa_m.sh
@@ -6,19 +6,19 @@ if [[ $1 = "--ros" ]] || [[ $1 = "-r" ]]
                   simulator=ros \
                   policy=rc_sarsa_m \
                   initial_conditions=3wrobot_kin_with_spot \
-                  +policy.R1_diag="[10, 100, 1e-1, 0, 0]" \
+                  +policy.R1_diag="[100, 100, 1, 0, 0]" \
                   scenario=my_scenario \
                   system=3wrobot_kin_with_spot \
                   common.sampling_time=0.1 \
                   simulator.time_final=50 scenario.N_iterations=50 \
+                  --single-thread \
                   --jobs=-1 \
-                  --experiment=sarsa_m_init_ros \
+                  --experiment=sarsa_m_report \
                   policy.critic_desired_decay=1e-6 \
                   policy.critic_low_kappa_coeff=1e-1 \
                   policy.critic_up_kappa_coeff=5e2 \
-                  policy.penalty_factor=1e3 \
-                  policy.step_size_multiplier=5 \
-                  --interactive
+                  policy.penalty_factor=1e2 \
+                  policy.step_size_multiplier=5
     done
     else
         python3.10 run.py +seed=7 \
