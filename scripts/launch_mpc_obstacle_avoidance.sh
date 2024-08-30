a8e11b17ef80dc23ec3705071b5ce037c8ff37c4
HuyHoang <huyhoang251296@gmail.com>
2024-08-30 08:45:15 +0200

    run MPC again

--- scripts/launch_mpc_obstacle_avoidance.sh
+++ scripts/launch_mpc_obstacle_avoidance.sh
@@ -7,7 +7,7 @@ if [[ $1 = "--ros" ]] || [[ $1 = "-r" ]]
                     system=3wrobot_kin_with_spot \
                     scenario=mpc_scenario_customized \
                     scenario.running_objective.spot_gain=100 \
-                    scenario.prediction_horizon=25 \
+                    scenario.prediction_horizon=10 \
                     scenario.prediction_step_size=4 \
                     common.sampling_time=.1 \
                     simulator.time_final=40 \
