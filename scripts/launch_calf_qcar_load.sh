576aae155af4af69de506a94d6c0a3ac5f628560
HuyHoang <huyhoang251296@gmail.com>
2024-09-23 08:37:01 +0200

    update qcar loading script

---
+++ scripts/launch_calf_qcar_load.sh
@@ -0,0 +1,18 @@
+python3.10 run.py \
+            +seed=10 \
+            simulator=ros_qcar \
+            policy=rc_calf_qcar \
+            scenario=calf_scenario \
+            system=qcar_kin \
+            common.sampling_time=0.1 \
+            simulator.time_final=50 scenario.N_iterations=1 \
+            --jobs=1 \
+            --experiment=calf_qcar_load \
+            policy.critic_desired_decay=1e-6 \
+            policy.critic_low_kappa_coeff=1e-1 \
+            policy.critic_up_kappa_coeff=8e2 \
+            policy.penalty_factor=5e2 \
+            policy.step_size_multiplier=1 \
+            policy.nominal_only=false \
+            policy.weight_path="/regelum-playground/regelum_data/outputs/2024-09-20/14-02-01/0/.callbacks/PolicyNumpyModelSaver/model_it_00012.npy" \
+            --iteractive
