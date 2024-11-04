84ae08d964ece4dc04fb139e87f7d92439253bbd
Huy Hoang <ngoc.hoang@stud.th-deg.de>
2024-11-04 13:53:12 +0100

    feat(notebook): save exp config

--- notebooks/utils/load_data.py
+++ notebooks/utils/load_data.py
@@ -8,7 +8,8 @@ import pandas as pd
 
 from utils.load_config import (
     get_df_historical_data,
-    get_list_historical_data
+    get_list_historical_data,
+    load_exp_config
     )
 
 
@@ -118,6 +119,7 @@ def get_df_from_datetime_range(start_datetime_str,
                 continue
 
             tmp_df["absolute_path"] = iteration_path
+            tmp_df["exp_config"] = load_exp_config(exp_path)
             
             if objective_function is not None:
                 tmp_df["objective_value"] = tmp_df.apply(lambda x: cal_obj_df(x, objective_function), axis=1)
