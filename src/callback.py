f57aefc209e006ab7248d1a133d2636a60ffe9c3
Huy Hoang <ngoc.hoang@stud.th-deg.de>
2024-11-09 09:30:09 +0100

    add log and notebook

--- src/callback.py
+++ src/callback.py
@@ -55,6 +55,7 @@ class GZHistoricalDataCallback(HistoricalDataCallback):
                         "task_name": output["task_name"],
                         "step_id": output["step_id"],
                         "phase": output["phase"],
+                        "exploration": output["exploration"],
                     },
                     **dict(zip(self.action_components_naming, output["action"][0])),
                     **dict(
