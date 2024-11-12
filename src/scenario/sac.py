9a4bb0b0c2f29cab9e622bb7a5a8dc49067ef34b
Huy Hoang <ngoc.hoang@stud.th-deg.de>
2024-11-12 09:42:43 +0100

    check with brightness and contrast enhancement

--- src/scenario/sac.py
+++ src/scenario/sac.py
@@ -91,6 +91,7 @@ class Actor(nn.Module):
         )
 
     def forward(self, x):
+        x = np.clip(x * 1.5 / 255 + 30 / 255, 0, 1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         mean = self.fc_mean(x)
